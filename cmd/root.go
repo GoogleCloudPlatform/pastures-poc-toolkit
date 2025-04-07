28e1db5c255386052b646cbe857810c7753085c1
Matt Williams <williamsmt@users.noreply.github.com>
2025-04-07 14:46:28 -0400

    let the rewrite begin

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,11 @@
 /*
-Copyright © 2024 Google LLC
+Copyright © 2025 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package cmd
 
 import (
@@ -24,25 +23,27 @@ import (
 	"github.com/spf13/viper"
 )
 
-var (
-	cfgFile string
-	verbose bool
-)
+var cfgFile string
 
-// RootCmd represents the base command when called without any subcommands
-var RootCmd = &cobra.Command{
-	Use:   "pasture",
-	Short: "A POC toolkit for Google Cloud",
-	Long: "Pastures is a CLI toolkit that will create POC " +
-		"landing zones in your Google Cloud organization. It relies " +
-		"on the Cloud Foundation Fabric framework to establish a GCP " +
-		"foundation, and it will deploy each 'pasture' as a Sandbox project.",
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pastures-poc-toolkit",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := RootCmd.Execute()
+	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -51,26 +52,15 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Lets hide the pesky completion flag
-	RootCmd.CompletionOptions.HiddenDefaultCmd = true
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	RootCmd.PersistentFlags().
-		StringVar(
-			&cfgFile, "config", "",
-			"config file (default is $HOME/.pastures.yaml)",
-		)
-	RootCmd.PersistentFlags().
-		BoolVar(
-			&verbose, "verbose", false, "controls Terraform output verbosity",
-		)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pastures-poc-toolkit.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -83,10 +73,10 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".pastures" (without extension).
+		// Search config in home directory with name ".pastures-poc-toolkit" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".pastures")
+		viper.SetConfigName(".pastures-poc-toolkit")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
